f18b520f67a7d94e0dfa01a38169b2bd65d20108
John Collins <collins@braincorporation.com>
2021-03-15 10:10:28 +0000

    Workspace bin

--- sandbox_bootstrap.sh
+++ sandbox_bootstrap.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # export PYTHONPATH=${HOME}/Workspace/shining_software/src:${PYTHONPATH};
-export PATH=/opt/gnu/bin:${HOME}/.local/bin:${HOME}/.emacs.d/bin:${PATH};
+export PATH=/opt/gnu/bin:${HOME}/.local/bin:${HOME}/.emacs.d/bin:${HOME}/Workspace/bin:${PATH};
 ~/.emacs.d/bin/doom sync;
 
 exec "$@"
