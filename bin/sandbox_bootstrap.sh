40aec15f7ea7ecdfeb14a7ebbbbbd88c84e77f2a
John Collins <john.collins@braincorp.com>
2021-04-10 02:18:05 -0700

    Saving

--- bin/sandbox_bootstrap.sh
+++ bin/sandbox_bootstrap.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Remap caps to escape.
 # setxkbmap -option caps:escape
-xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+# xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
 
 # export PYTHONPATH=${HOME}/Workspace/shining_software/src:${PYTHONPATH};
 export PATH=/opt/gnu/bin:${HOME}/.local/bin:${HOME}/.emacs.d/bin:${HOME}/Workspace/bin:${PATH};
